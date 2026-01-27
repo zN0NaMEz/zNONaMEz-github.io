38f94ec69e03d1867374a2a9ce4ae79e2d2850eb
ajeentan <apirakjeentang@gmail.com>
2026-01-27 11:22:38 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,6 @@
-var button = document.querySelector(“button”);
-//attach and event when the user clicks it
-button.addEventListener(“click”, myfunction);
-//create the function that will be called when the
-button is pressed
-function myfunction()
-{
-//this shows a popup window
-alert(“button clicked!”);
+var button = document.querySelector("button"); 
+button.addEventListener("click", myfunction);
+
+function myfunction() {
+    alert("button clicked!");
 }
